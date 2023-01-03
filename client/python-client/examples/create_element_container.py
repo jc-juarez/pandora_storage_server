7ee29358afdaf719e42b075a660375f76223ecad
jc-juarez <jc.juarezgarcia@outlook.com>
2023-01-03 03:40:13 -0600

    Added new example

--- client/python-client/examples/create_element_container.py
+++ client/python-client/examples/create_element_container.py
@@ -6,11 +6,18 @@
 # Contact: jc.juarezgarcia@outlook.com
 # *************************************
 
-pandora_client = PandoraClient(throw_exceptions=False)
+import pandora_server_storage_client as pandora
 
-response = pandora_client.create_element_container('MyElementContainer')
+def main():
 
-if(response.error):
-    print('Response had error code: {0} | {1}'.format(response.pandora_error_code, response.pandora_error_message))
-else:
-    print('Succesful operation: {0}'.format(response.http_response_body))
+    pandora_client = pandora.PandoraClient(throw_exceptions=False)
+
+    response = pandora_client.create_element_container('MyElementContainer')
+
+    if(response.error):
+        print('Response had error code: {0} | {1}'.format(response.pandora_error_code, response.pandora_error_message))
+    else:
+        print('Succesful operation: {0}'.format(response.http_response_body))
+
+if __name__ == '__main__':
+    main()
---
+++ client/python-client/pandora_server_storage_client/__init__.py
@@ -0,0 +1,10 @@
+# *************************************
+# Pandora Storage Server Python Client
+# Client
+# '__init__.py'
+# Author: Juan Carlos Juárez
+# Contact: jc.juarezgarcia@outlook.com
+# *************************************
+
+from .pandora_client import PandoraClient
+from .pandora_client import ResponseObject
