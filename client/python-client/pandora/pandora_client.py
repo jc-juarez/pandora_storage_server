036b88a08d7e376c6ebe6baf2dd7a2d3f43f96cb
jc-juarez <jc.juarezgarcia@outlook.com>
2023-01-03 00:10:18 -0600

    Added Client

---
+++ client/python-client/pandora/pandora_client.py
@@ -0,0 +1,12 @@
+# *************************************
+# Pandora Storage Server Python Client
+# Client
+# 'pandora_client.py'
+# Author: Juan Carlos Juárez
+# Contact: jc.juarezgarcia@outlook.com
+# *************************************
+
+class PandoraClient:
+
+    def __init__(self):
+        
